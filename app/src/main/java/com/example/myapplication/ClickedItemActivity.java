28684a59a8e6e80ef92e4614219dd181cbb8088a
jerry3972 <54852021+jerry3972@users.noreply.github.com>
2021-07-01 23:19:36 +0900

    zoom failed

--- app/src/main/java/com/example/myapplication/ClickedItemActivity.java
+++ app/src/main/java/com/example/myapplication/ClickedItemActivity.java
@@ -2,12 +2,26 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import pl.polidea.view.ZoomView;
+
+import static java.lang.Math.abs;
+
 public class ClickedItemActivity extends AppCompatActivity  {
+
     ImageView imageView;
     TextView textView;
 
@@ -28,6 +42,24 @@ public class ClickedItemActivity extends AppCompatActivity  {
             textView.setText(selectedName);
             imageView.setImageResource(selectedImage);
         }
+        //setContentView(R.layout.fragment2_activity_main);
+
+//        View v= ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.fragment2_activity_clicked_item,null,false);
+//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
+//
+//        ZoomView zoomView = new ZoomView(this);
+//        zoomView.addView(v);
+//        zoomView.setLayoutParams(layoutParams);
+//        zoomView.setMiniMapEnabled(true); // 좌측 상단 검은색 미니맵 설정
+//        zoomView.setMaxZoom(4f); // 줌 Max 배율 설정  1f 로 설정하면 줌 안됩니다.
+//        zoomView.setMiniMapCaption("Mini Map Test"); //미니 맵 내용
+//        zoomView.setMiniMapCaptionSize(20); // 미니 맵 내용 글씨 크기 설정
+//
+//        RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
+//        container.addView(zoomView);
+
 
     }
+
+
 }
