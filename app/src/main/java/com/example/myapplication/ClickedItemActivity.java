0900e4e01a98940586935b8078364a7cc30e94e7
jerry3972 <54852021+jerry3972@users.noreply.github.com>
2021-07-02 11:30:54 +0900

    zoom success~~!

--- app/src/main/java/com/example/myapplication/ClickedItemActivity.java
+++ app/src/main/java/com/example/myapplication/ClickedItemActivity.java
@@ -16,6 +16,9 @@ import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
 import pl.polidea.view.ZoomView;
 
 import static java.lang.Math.abs;
@@ -30,7 +33,7 @@ public class ClickedItemActivity extends AppCompatActivity  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment2_activity_clicked_item);
 
-        imageView = findViewById(R.id.imageView);
+        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         textView = findViewById(R.id.tvName);
 
         Intent intent = getIntent();
@@ -40,24 +43,8 @@ public class ClickedItemActivity extends AppCompatActivity  {
             int selectedImage = intent.getIntExtra("image",0);
 
             textView.setText(selectedName);
-            imageView.setImageResource(selectedImage);
+            imageView.setImage(ImageSource.resource(selectedImage));
         }
-        //setContentView(R.layout.fragment2_activity_main);
-
-//        View v= ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.fragment2_activity_clicked_item,null,false);
-//        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.MATCH_PARENT);
-//
-//        ZoomView zoomView = new ZoomView(this);
-//        zoomView.addView(v);
-//        zoomView.setLayoutParams(layoutParams);
-//        zoomView.setMiniMapEnabled(true); // 좌측 상단 검은색 미니맵 설정
-//        zoomView.setMaxZoom(4f); // 줌 Max 배율 설정  1f 로 설정하면 줌 안됩니다.
-//        zoomView.setMiniMapCaption("Mini Map Test"); //미니 맵 내용
-//        zoomView.setMiniMapCaptionSize(20); // 미니 맵 내용 글씨 크기 설정
-//
-//        RelativeLayout container = (RelativeLayout) findViewById(R.id.container);
-//        container.addView(zoomView);
-
 
     }
 
