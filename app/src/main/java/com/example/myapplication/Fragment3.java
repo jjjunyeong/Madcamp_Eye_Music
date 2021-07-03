7e7346146e07bf94d4b4309159ad203239f63b12
jjjunyeong <jjjunyeong@gmail.com>
2021-07-03 20:01:31 +0900

    add thread

--- app/src/main/java/com/example/myapplication/Fragment3.java
+++ app/src/main/java/com/example/myapplication/Fragment3.java
@@ -36,11 +36,14 @@ public class Fragment3 extends Fragment {
     public final static int RECORD_PERM_CODE = 103;
     private int mAudioSource = MediaRecorder.AudioSource.MIC;
     private int mSampleRate = 8000; //frequency
-    private int mChannelCount = AudioFormat.CHANNEL_IN_MONO; //channelConfiguration
+    private int mChannelCount = AudioFormat.CHANNEL_IN_STEREO; //channelConfiguration
     private int mAudioFormat = AudioFormat.ENCODING_PCM_16BIT; //audioEncoding
     private int mBufferSize = AudioTrack.getMinBufferSize(mSampleRate, mChannelCount, mAudioFormat); //buffersize
 
     //RecordAudio recordTask;
+    AudioRecord audioRecord = null;
+
+    public Thread mRecordThread = null;
 
     private FFT transformer;
     int blockSize = 256;
@@ -100,48 +103,53 @@ public class Fragment3 extends Fragment {
             started = true;
             recordButton.setText("ON_RECORD");
             recordTask();
-            //recordTask.execute();
         }
     }
 
     private void recordTask(){
-        AudioRecord audioRecord = new AudioRecord( MediaRecorder.AudioSource.MIC, mSampleRate, mChannelCount, mAudioFormat, mBufferSize);
-        short[] buffer = new short[blockSize]; //blockSize = 256
-        double[] toTransform = new double[blockSize]; //blockSize = 256
+        mRecordThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                AudioRecord audioRecord = new AudioRecord( MediaRecorder.AudioSource.MIC, mSampleRate, mChannelCount, mAudioFormat, mBufferSize);
+                short[] buffer = new short[blockSize]; //blockSize = 256
+                double[] toTransform = new double[blockSize]; //blockSize = 256
 
-        audioRecord.startRecording();
+                audioRecord.startRecording();
 
-        while (started) {
-            int bufferReadResult = audioRecord.read(buffer, 0, blockSize); //blockSize = 256
-            Log.i("bufferReadResult", Integer.toString(bufferReadResult));
-            // AudioRecord 객체에서 데이터를 읽은 다음에는 short 타입의 변수들을 double 타입으로
-            // 바꾸는 루프를 처리한다.
-            // 직접 타입 변환(casting)으로 이 작업을 처리할 수 없다. 값들이 전체 범위가 아니라 -1.0에서
-            // 1.0 사이라서 그렇다
-            // short를 32,767(Short.MAX_VALUE) 으로 나누면 double로 타입이 바뀌는데,
-            // 이 값이 short의 최대값이기 때문이다.
-            for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
-                toTransform[i] = (double) buffer[i] / Short.MAX_VALUE; // 부호 있는 16비트
-                Log.i("buffer", Double.toString(buffer[i]));
-                Log.i("Short.MAX_VALUE", Short.toString(Short.MAX_VALUE));
-                Log.i("toTransform", Double.toString(toTransform[i]));
+                while (started) {
+                    int bufferReadResult = audioRecord.read(buffer, 0, blockSize); //blockSize = 256
+                    Log.i("bufferReadResult", Integer.toString(bufferReadResult));
+                    // AudioRecord 객체에서 데이터를 읽은 다음에는 short 타입의 변수들을 double 타입으로
+                    // 바꾸는 루프를 처리한다.
+                    // 직접 타입 변환(casting)으로 이 작업을 처리할 수 없다. 값들이 전체 범위가 아니라 -1.0에서
+                    // 1.0 사이라서 그렇다
+                    // short를 32,767(Short.MAX_VALUE) 으로 나누면 double로 타입이 바뀌는데,
+                    // 이 값이 short의 최대값이기 때문이다.
+                    for (int i = 0; i < blockSize && i < bufferReadResult; i++) {
+                        toTransform[i] = (double) buffer[i] / Short.MAX_VALUE; // 부호 있는 16비트
+                        Log.i("buffer", Double.toString(buffer[i]));
+                        Log.i("Short.MAX_VALUE", Short.toString(Short.MAX_VALUE));
+                        Log.i("toTransform", Double.toString(toTransform[i]));
+                    }
+
+                    // 이제 double값들의 배열을 FFT 객체로 넘겨준다. FFT 객체는 이 배열을 재사용하여 출력 값을
+                    // 담는다. 포함된 데이터는 시간 도메인이 아니라
+                    // 주파수 도메인에 존재한다. 이 말은 배열의 첫 번째 요소가 시간상으로 첫 번째 샘플이 아니라는 얘기다.
+                    // 배열의 첫 번째 요소는 첫 번째 주파수 집합의 레벨을 나타낸다.
+
+                    // 256가지 값(범위)을 사용하고 있고 샘플 비율이 8,000 이므로 배열의 각 요소가 대략
+                    // 15.625Hz를 담당하게 된다. 15.625라는 숫자는 샘플 비율을 반으로 나누고(캡쳐할 수 있는
+                    // 최대 주파수는 샘플 비율의 반이다. <- 누가 그랬는데...), 다시 256으로 나누어 나온 것이다.
+                    // 따라서 배열의 첫 번째 요소로 나타난 데이터는 영(0)과 15.625Hz 사이에
+                    // 해당하는 오디오 레벨을 의미한다.
+                    //transformer.fft(toTransform);
+                }
+                audioRecord.stop();
             }
-
-            // 이제 double값들의 배열을 FFT 객체로 넘겨준다. FFT 객체는 이 배열을 재사용하여 출력 값을
-            // 담는다. 포함된 데이터는 시간 도메인이 아니라
-            // 주파수 도메인에 존재한다. 이 말은 배열의 첫 번째 요소가 시간상으로 첫 번째 샘플이 아니라는 얘기다.
-            // 배열의 첫 번째 요소는 첫 번째 주파수 집합의 레벨을 나타낸다.
-
-            // 256가지 값(범위)을 사용하고 있고 샘플 비율이 8,000 이므로 배열의 각 요소가 대략
-            // 15.625Hz를 담당하게 된다. 15.625라는 숫자는 샘플 비율을 반으로 나누고(캡쳐할 수 있는
-            // 최대 주파수는 샘플 비율의 반이다. <- 누가 그랬는데...), 다시 256으로 나누어 나온 것이다.
-            // 따라서 배열의 첫 번째 요소로 나타난 데이터는 영(0)과 15.625Hz 사이에
-            // 해당하는 오디오 레벨을 의미한다.
-            //transformer.fft(toTransform);
-        }
-        audioRecord.stop();
+        });
 
 
     }
+
 
 }
